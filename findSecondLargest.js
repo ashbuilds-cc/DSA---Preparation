41d038a73debf31c95178427bc359c1cf983ecd9
Ashish Kumar Mohan <ashishkumarmohan5@gmail.com>
2026-04-28 23:07:14 +0530

    Second Largest Number from an Array

---
+++ findSecondLargest.js
@@ -0,0 +1,21 @@
+function findSecondLargest(numbers) {
+    if (!numbers.length || numbers.length < 2) {
+        return -1;
+    }
+
+    let secondLargest = [-Infinity, -Infinity];
+
+    for (let number of numbers) {
+        if (number > secondLargest[0]) {
+            secondLargest[1] = secondLargest[0];
+            secondLargest[0] = number;
+        }
+        else if (number > secondLargest[1] && number !== secondLargest[0]) {
+            secondLargest[1] = number;
+        }
+    }
+
+    return secondLargest[1] === -Infinity ? -1 : secondLargest[1];
+}
+
+console.log(findSecondLargest([1, 2, 3, 3,3 , 5, 9, 0, 9]));
